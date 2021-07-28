b8f71380435677b2a52a7cf17c0055e333f6bb47
Humberto Piva <contato.humbertopiva@gmail.com>
2021-07-28 20:05:01 -0300

    Primeiro exemplo de promise

---
+++ funcoes/promise_1.js
@@ -0,0 +1,16 @@
+let p = new Promise(function(resolve){
+    resolve(['Ana', 'Bia', 'Carlos', 'Daniel']);
+})
+
+const primeiroElemento = arrayOuString => arrayOuString[0];
+const letraMinuscula = letra => letra.toLowerCase();
+
+
+
+
+p
+
+    .then(primeiroElemento)
+    .then(primeiroElemento)
+    .then(letraMinuscula)
+    .then(console.log);
