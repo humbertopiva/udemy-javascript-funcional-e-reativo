ffd560c150a29e5165935c50db1dbd7e72e6e64d
Humberto Piva <contato.humbertopiva@gmail.com>
2021-07-29 17:04:02 -0300

    Posicionando os tratamentos de erros

---
+++ funcoes/desafio_3.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+function lerArquivo(caminho) {
+    
+    return new Promise(resolve => {
+        const file = fs.readFile(caminho, function(_, conteudo){
+            resolve(conteudo.toString());
+        })
+    })
+}
+
+const caminho = path.join(__dirname, 'dados.txt');
+lerArquivo(caminho)
+
+  .then(conteudo => conteudo.split(/\r?\n/))
+  .then(linhas => linhas.join(','))
+  .then(conteudo => `O valor final é: ${conteudo}`)
+  .then(console.log);
---
+++ funcoes/promise_5.js
@@ -0,0 +1,25 @@
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        try{
+            con.log('temp')
+            if(Math.random() < chanceErro) {
+                reject('Ocorreu um erro!')
+            } else {
+                resolve(valor)
+            }
+
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+funcionarOuNao('testando...', 0.9)
+    .then(v => console.log(`Valor: ${v}`))
+    .then(
+        v => consol.log(v),
+        err => console.log(`Erro Esp.: ${err}`)
+    )
+    .then(() => console.log('Quase o fim'))
+    .catch(err => console.log(`Erro Geral: ${err}`))
+    .then(() => console.log('Fim!'))
