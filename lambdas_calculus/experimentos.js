92851192f160e3c1cee3ed1bcbe85f41b102feac
Humberto Piva <contato.humbertopiva@gmail.com>
2021-08-25 21:17:50 -0300

    Lambda Calculus - Função Troca(FLIP)

---
+++ lambdas_calculus/experimentos.js
@@ -0,0 +1,39 @@
+let r
+
+const I = a => a
+
+r = I(3)
+r
+
+r = I(I)
+r
+
+const SELF = f => f(f)
+
+r = SELF(I)
+r
+
+const PRI = a => _ => a
+
+r = PRI(7)(11)
+r
+
+const ULT = _ => b => b
+
+r = ULT(7)(11)
+r
+
+const TRO = f => a => b => f(b)(a)
+
+r = TRO(ULT)(7)(11)
+r
+
+r = TRO(PRI)(6)(12)
+r
+
+const ULT2 = a => b => TRO(PRI)(a)(b)
+
+r = ULT2(13)(20)
+r
+
+// boolean TRUE e FALSE
